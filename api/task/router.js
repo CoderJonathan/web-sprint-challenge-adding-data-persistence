311b6f8f661b6c5eb02cfa0113403ed75f8f1cf1
Jonathan Garcia <714JonathanreneGarcia@gmail.com>
2021-07-16 10:32:40 -0700

    MVP

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,26 @@
 // build your `/api/tasks` router here
+
+const router = require('express').Router()
+const {logger, checkTaskPayload} = require('./middleware')
+const Tasks = require('./model')
+
+
+// [ ] [GET] /api/tasks
+router.get('/', logger, (req, res, next) => {
+    Tasks.getAll(req.query)
+    .then(tasks => {
+        res.status(200).json(tasks)
+    })
+    .catch(err => next())
+})
+
+// [ ] [POST] /api/tasks
+router.post('/', logger, checkTaskPayload, (req, res, next) => {
+    Tasks.addTask(req.body)
+    .then(task => {
+        res.status(200).json(task)
+    })
+    .catch(next)
+})
+
+module.exports = router
